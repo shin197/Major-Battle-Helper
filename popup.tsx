27d30137c4658ec9c2d8fd362e4e46ff4f66d4c7
신재혁 <y_spk@naver.com>
2025-06-16 00:24:00 +0900

    chore: init plasmo extension

--- popup.tsx
+++ popup.tsx
@@ -9,11 +9,7 @@ function IndexPopup() {
         padding: 16
       }}>
       <h2>
-        Welcome to your{" "}
-        <a href="https://www.plasmo.com" target="_blank">
-          Plasmo
-        </a>{" "}
-        Extension!
+        Major Battle Helper
       </h2>
       <input onChange={(e) => setData(e.target.value)} value={data} />
       <a href="https://docs.plasmo.com" target="_blank">
