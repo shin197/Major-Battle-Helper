9323b0a247bc1d1580b68a27d5225fe94921c58a
Skywindragoon <spk77k@gmail.com>
2026-02-19 03:03:32 +0900

    chore: enter-eval 2개로 변경
    fix: major-battle index 수정

--- contents/dev-wrapper.ts
+++
@@ -1,1 +0,0 @@
-// import "../major-battle/index"
--- major-battle/index.ts
+++ major-battle/index.ts
@@ -2,6 +2,6 @@ import "./dice-roll13.0"
 import "./some-script"
 import "./dice-result"
 import "./dice-var-exp"
-import "./enter-eval"
+import "./enter-eval-mb"
 
 console.info("Major Battle Script Loaded")
